c1a41c8e8c1dda6b48b5965880c1b1139745c5dc
feiyu <zfeiyu@mail.ustc.edu.cn>
2021-11-23 10:33:57 +0800

    handle assignment to self in operator=

---
+++ code/effective_cpp/test_swap.cpp
@@ -0,0 +1,11 @@
+#include <iostream>     
+#include <algorithm>    // std::swap
+using namespace std;
+
+// swap(a, b) : Exchanges the values of a and b.
+// http://www.cplusplus.com/reference/algorithm/swap/
+
+int main() {
+	// todo
+	return 0;
+}
